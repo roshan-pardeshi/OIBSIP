2daf1091d4b1c097f13d12977c990942e8bdf457
roshan <roshanpardeshi926@gmail.com>
2026-07-31 14:42:42 +0530

    day 20 commit

---
+++ day20.py
@@ -0,0 +1,347 @@
+
+
+name = "roshan"
+
+##print(name[0])
+##print(name[4])
+##print(name[3])
+
+
+##for i in range(2,21,2):
+##    print("even",i)
+
+
+
+    
+##product = "mobile phone"
+##
+##for i in range(0,12,1):
+##    print(product[i])
+##    
+
+##num = 5
+##def A():
+##    for i in range(1,11,1):
+##        print(num*i)
+##
+##A()
+
+
+##class A:
+##
+##    def B(self):
+##        for i in range(1,50,1):
+##            print(i)
+##
+##a = A()
+##a.B()
+
+
+##class A:
+##
+##    def square(self,num):
+##        for i in range(1,21,1):
+##            print(i)
+##
+##
+##a = A()
+##a.square(5)
+##
+
+
+
+
+##def square(num):
+##    s = num * num
+##
+##    print(s)
+##
+##
+##for i in range(1,20,1):
+##    square(i)
+
+
+
+
+##for i in range(2,21,2):
+##    print(i)
+
+
+##class B:
+##
+##    def even_number(self,num):
+##
+##        for i in range(1,num+1,1):
+##            print(i)
+##
+##
+##b = B()
+##b.even_number(25)
+
+class number:
+
+    def print_number(self):
+        for i in range(1,11,1):
+            print(i)
+
+
+
+##a = number()
+##a.print_number()
+##
+
+
+
+class reverse_number:
+
+    def reverse_number_m(self):
+        for i in range(10,1,-1):
+            print(i)
+
+
+##a = reverse_number()
+##a.reverse_number_m()
+
+
+
+class even_number:
+
+    def even(self):
+        for i in range(2,21,2):
+            print(i)
+##
+##a = even_number()
+##a.even()
+##
+
+
+##class odd_number:
+##
+##    def odd(self):
+##        for i in range(1,20,2):
+##            print("odd number",i)
+##
+##a = odd_number()
+##a.odd()
+
+
+##class natural:
+##
+##    def natural_number(self):
+##        for i in range(1,20,1):
+##            print(i)
+##
+##a = natural()
+##a.natural_number()
+
+##num = int(input("enter the number"))
+
+
+##class table:
+##    
+##    def method_table(self):
+##        for i in range(1,11,1):
+##            print(num * i)
+##
+##a = table()
+##a.method_table()
+##        
+
+
+##class table:
+##
+##    def display_table(self,num):
+##        for i in range(1,11,1):
+##            print(num*i)
+##
+##
+##a = table()
+##a.display_table(3)
+
+##class table:
+##    def all_tables(self):
+##
+##        for i in range(2,6,1):
+##            print(i*1)
+##            print(i*2)
+##            print(i*3)
+##            print(i*4)
+##            print(i*5)
+##            print(i*6)
+##            print(i*7)
+##            print(i*8)
+##            print(i*9)
+##            print(i*10)
+##            
+##            
+##
+##
+##
+##a = table()
+##a.all_tables()
+
+
+
+##class rev:
+##
+##
+##    def rev_no(self,num):
+##        for i in range(10,0,-1):
+##            print(num*i)
+##
+##
+##a = rev()
+##a.rev_no(7)
+
+
+
+##class square:
+##
+##    def squ(self):
+##
+##        for i in range(1,11,1):
+##            print(i**2)
+##
+##
+##
+##a= square()
+##a.squ()
+
+
+class word:
+
+    def display_c(self,name):
+
+        for i in range(0,6,1):
+            print(name[i])
+
+##a = word()
+##a.display_c("python")
+##        
+
+
+
+class course:
+
+    def course_name(self,name):
+        for i in range(0,12,1):
+            print(name[i])
+
+##a = course()
+##a.course_name("data science")
+##
+
+
+class country:
+
+    def country_name(self,country_n):
+        for i in range(0,5,1):
+            print(country_n[i])
+
+##a = country()
+##a.country_name("india")
+
+
+
+##class star_p:
+##    def pattern(self):
+##
+##        for i in range(1,5,1):
+##            print("*" * i)
+##
+##
+##a = star_p()
+##a.pattern() 
+
+
+
+##class star_pp:
+##
+##    def sp(self):
+##        for i in range(0,4,-1):
+##            print(str(i)*i)
+##
+##
+##a = star_pp()
+##a.sp()
+
+
+##class sum_n:
+##    
+##    def sum1(self):
+##        sum1 = 0
+##        for i in range(1,101,1):
+##            sum1 = sum1 + i
+##
+##        print(sum1)
+##
+##a = sum_n()
+##a.sum1()
+
+
+name_c = "python"
+
+class count_d:
+
+    def count_down(self):
+        for i in range(5,0,-1):
+            print(name_c[i])
+
+##a = count_d()
+##a.count_down()
+
+
+
+##class count:
+##    def method(self):
+##        for i in range(20,1,-1):
+##            print(i)
+##
+##
+##a = count()
+##a.method()
+            
+
+print("hi" * 6)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day_sunday_task.py
@@ -0,0 +1,33 @@
+
+
+##print("hellow")
+
+
+##from math import *
+##
+##print(factorial(5))
+
+
+def A():
+    print("hellow")
+
+    def B():
+        print("hi")
+
+    def C():
+        print("heyyy")
+
+    return B,C
+
+##a,b= A()
+##a()
+##b()
+
+
+##for i in range(1,20):
+##    print(i)
+
+for i in range(1,20):
+    if i % 2==0:
+        print(i)
+
