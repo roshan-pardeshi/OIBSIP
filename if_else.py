f058ff42ed567df44b8978d235677c6b4335b04d
roshan <roshanpardeshi926@gmail.com>
2026-08-04 22:23:36 +0530

    added one more file

---
+++ if_else.py
@@ -0,0 +1,128 @@
+
+
+
+
+##num = 1
+##if num>0:
+##    print("positive number")
+
+
+##student = 54
+##
+##
+##if student>90:
+##    print("the student have excellent marks :-",student)
+
+
+##num = 29
+##
+##if num % 5 == 0:
+##    print("the number is divisiable by 5:***")
+##
+##    
+
+##year = 369
+##
+##if year>365:
+##    print("the year is a leap year")
+
+
+##word = "python"
+##
+##
+##if word == "pytho":
+##    print("the word is python ")
+##
+##temp  = 46
+##
+##
+##if temp>45:
+##    print("hottt")
+##else:
+##    print("cool")
+##    
+##num = -8
+##if num >0:
+##    print("the number is positive")
+##
+##else:
+##    print("the number is anegative")
+
+##
+##
+##name  = input("enter the string")
+##
+##if name == "admin":
+##    print("welcome admin saheb")
+##    
+####else:
+####    print("other one is access")
+####
+
+
+##num = int(input("enter the number"))
+##
+##
+##if num >0:
+##    print("the number is a positive")
+##elif(num==0):
+##    print("the number is a zero")
+##else:
+##    print("the number is a negative")
+
+
+##student = int(input("enter the student marks"))
+##
+##if student>90:
+##    print("the student have A grade")
+##elif(student > 75 and student < 85):
+##    print("student have a B grade")
+##elif(student > 50 and student < 74):
+##    print("the student have a C grade")
+##else:
+##    print("the student is fail")
+     
+##age = int(input("enter the number"))
+##
+##if age < 13:
+##    print("childern")
+##elif(age > 13 and age < 19):
+##    print("teanger people")
+##elif(age >20 and age < 45):
+##    print("most tergel in life")
+##else:
+##    print("senior citize people")
+
+
+word = input("enter the number")
+
+##a1 = ['a','o','e','i']
+
+if word == 'a' or word =='e':
+    print("vowel word")
+else:
+    print("not vowel word")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
