f5e2b39dc4b18fd9d51970a80182bf599dca0bd1
roshan <roshanpardeshi926@gmail.com>
2026-08-13 23:33:06 +0530

    add one more

--- nested_loop.py
+++ nested_loop.py
@@ -147,7 +147,7 @@
 ##*   *
 ##*   *
 ##*****
-##
+
 ##for i in range(1,4,1):
 ##    for j in range(1,5,1):
 ##        print("*",end="") 
@@ -156,7 +156,7 @@
 ##    if i==3:
 ##        if i==5:
 ##            print("*" * 5)
-
+##
 
 
 
@@ -187,25 +187,407 @@
 
 
 
+##for i in range(1,5,1):
+##    for j in range(1,5,1):
+##        if j==1 or j==4:
+##            print("*",end="")
+##        else:
+##            if i==1 or i==4:
+##                print("*",end="")
+##            else:
+##                print(end=" ")
+##
+##    print("  ")
+##
+
+##
+##for i in range(1,6,1):
+##    for j in range(0,4,1):
+##        if  j==1 or  j==2 or j==3:
+##            print("*",end="")
+##        if i==1 or i==5:
+##            print("*",end="")
+##        else:
+##        
+##            print(" ",end="")
+####            continue
+##    print()
+####
+
 for i in range(1,5,1):
     for j in range(1,5,1):
         if j==1 or j==4:
-            print("*",end="")
+            print(j,end="")
         else:
             if i==1 or i==4:
-                print("&",end="")
+                print(j,end="")
             else:
                 print(end=" ")
 
-    print("  ")
+    print()
 
-
-
+##print("\n")
+##
 ##for i in range(1,5,1):
-##    for i in range(1,5,1):
-##        print("*",end="")
+##    for j in range(1,5,1):
+##        if j==1 or j==4:
+##            print(i,end="")
+##        else:
+##            if i==1 or i==4:
+##                print(i,end="")
+##            else:
+##                print(" ",end="")
+##    print()
+####
+
+##
+
+##a="*"
+##for i in range(5,0,-1):
+####    print(i)
+##    for k in range(5,0,-1):
+##        print(a,end="")
+##    print()
+##
+##    for j in range(1,i+1,1):
+##        print(" ",end=" ")
+
+             
+   
+
+##a="*"
+##for i in range(5,0,-1):
+####    print(i)
+##    for k in range(5,0,-1):
+##        print(a,end=" ")
+##    print()
+##
+##    for j in range(1,i+1,1):
+##        print(" ",end="")
+##
+
+##  *
+## * *
+##* * *
+
+##c = 4
+##
+##for i in range(1,5,1):
+##
+##    
+##    for k in range(1,i):
+##        if i==1 or  k==1:
+##            print("*",end=" ")
+##            c-=1
+##        elif i==3 or k==3:
+##            print("*",end=" ")
+##            
+##        elif i==4 or k==4:
+##            print("*",end=" ")
+##            c-=1
+##        else:
+##            print("--")
 ##
 ##    print()
+##    j=1
+##    for j in range(c,j,-1):
+##            print(" ",end="")
+            
+        
+            
+##    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+##    for k in range(1,i):
+##        print("*",end="")
+
+##for i in range(1,4,1):
+##    for j in range(0,5,1):
+##        print("*",end="")
+##
+##    if j==1 or j==2:
+##        for k in range(1,i+1,1):
+##            print("")
+##            
+##    print()
+
+
+##****
+##*  *
+##*  *
+##****
+
+##for i in range(1,5,1):
+##    for j in range(1,5,1):
+##        if j==1 or j==4:
+##            print("*",end="")
+##        else:
+##            if i==1 or i==4:
+##                print("*",end="")
+##            else:
+##                print(" ",end="")
+##
+##    print()
+
+
+
+
+##c = 4
+##
+##for i in range(1,7,1):
+##
+##    for k in range(1,i):
+##        if i==1 or  k==1:
+##            print("*",end=" ")
+##            c-=1
+##        elif i==3 or k==3:
+##            print("*",end=" ")
+##            
+##        elif i==4 or k==4:
+##            print("*",end=" ")
+##            c-=1
+##        else:
+##            print("")
+##
+##    print()
+##    j=1
+##    for j in range(c,j,-1):
+##            print(" ",end="")
+##
+####d = 4
+##
+##for e in range(5,0,-1):
+##
+##    
+##    for w in range(1,i):
+##        if e==1 or  w==1:
+##            print("*",end=" ")
+##            d-=1
+##        elif e==3 or w==3:
+##            print("*",end=" ")
+##            
+##        elif e==4 or w==4:
+##            print("*",end=" ")
+##            d-=1
+##        else:
+##            print("")
+##
+##    print()
+##    t=1
+##    for t in range(d,t,-1):
+##            print(" ",end="")
+
+##print("hi")
+##                   * 
+##                  * * 
+##                 * * * 
+##                * * * * 
+##               * * * * * 
+##              * * * * * * 
+##             * * * * * * * 
+##              * * * * * * 
+##               * * * * * 
+##                * * * * 
+##                 * * * 
+##                  * * 
+##                   * 
+                    
+                    
+
+
+c = 20
+
+for i in range(1,8,1):
+
+    for k in range(1,i):
+        if i==1 or  k==1:
+            print("*",end=" ")
+            c-=1
+        elif i==3 or k==3:
+            print("*",end=" ")
+            
+        elif i==4 or k==4:
+            print("*",end=" ")
+##            c-=1
+            
+        elif i==5 or k==5:
+            print("*",end=" ")
+            
+        elif i==6 or k==6:
+            print("*",end=" ")
+            
+        elif i==7 or k==7:
+            print("*",end=" ")
+##            c-=1
+
+        elif i==8 or k==8:
+            print("*",end=" ")
+##            c-=1
+
+        elif i==9 or k==9:
+            print("&",end=" ")
+##            c-=1
+##        
+##        
+##            
+##        else:
+##            print("")
+##
+##    print()
+##    j=1
+##    for j in range(c,j,-1):
+##            print(" ",end="")
+##
+##
+##d = 13
+##
+##for r in range(8,0,-1):
+##
+##    for t in range(1,r):
+##        if r==1 or  t==1:
+##            print("*",end=" ")
+##            d+=1
+##        elif r==2 or t==2:
+##            print("*",end=" ")
+##            
+##        elif r==3 or t==3:
+##            print("*",end=" ")
+##            
+##        elif r==4 or t==4:
+##            print("*",end=" ")
+####            c-=1
+##            
+##        elif r==5 or t==5:
+##            print("*",end=" ")
+##            
+##        elif r==6 or t==6:
+##            print("*",end=" ")
+##
+##        elif r==7 or t==7:
+##            print("*",end=" ")
+##            
+##        elif r==8 or t==8:
+##            print("*",end=" ")
+####            c-=1
+##
+##        elif r==9 or t==9:
+##            print("*",end=" ")
+##            d-=1
+##
+##        elif r==9 or t==9:
+##            print("*",end=" ")
+####            c-=1
+##        
+##        
+##            
+##        else:
+##            print(" ")
+##
+##    print()
+##    j=0
+####    d=1
+##    for j in range(d,j,-1):
+##            print(" ",end="")
+
+
+
+
+c = 20
+
+for i in range(1,8,1):
+
+    for k in range(1,i):
+        if i==1 or  k==1:
+            print("*",end=" ")
+            c-=1
+        elif i==3 or k==3:
+            print(" ",end=" ")
+            
+        elif i==4 or k==4:
+            print(" ",end=" ")
+##            c-=1
+            
+        elif i==5 or k==5:
+            print(" ",end=" ")
+            
+        elif i==6 or k==6:
+            print(" ",end=" ")
+            
+        elif i==7 or k==7:
+            print(" ",end=" ")
+##            c-=1
+
+        elif i==8 or k==8:
+            print(" ",end=" ")
+##            c-=1
+
+        
+        
+        
+            
+        else:
+            print("")
+
+    print()
+    j=1
+    for j in range(c,j,-1):
+            print(" ",end="")
+
+
+d = 13
+
+##for r in range(8,0,-1):
+##
+##    for t in range(1,r):
+##        if r==1 or  t==1:
+##            print("*",end=" ")
+##            d+=1
+##        elif r==2 or t==2:
+##            print(" ",end=" ")
+##            
+##        elif r==3 or t==3:
+##            print(" ",end=" ")
+##            
+##        elif r==4 or t==4:
+##            print(" ",end=" ")
+####            c-=1
+##            
+##        elif r==5 or t==5:
+##            print(" ",end=" ")
+##            
+##        elif r==6 or t==6:
+##            print(" ",end=" ")
+##
+##        elif r==7 or t==7:
+##            print(" ",end=" ")
+##            
+##        
+##            
+##        else:
+##            print(" ")
+##
+##    print()
+##    j=0
+####    d=1
+##    for j in range(d,j,-1):
+##            print(" ",end="")
+##
+##
+##
+
+
+    
 
 
 
---
+++ nested_loop_ravi_sir.py
@@ -0,0 +1,303 @@
+##
+##
+####for i in range(1,4,1):
+####    print(" "*(4-i),"*"*((2*i)-1))
+####
+####for i in range(5,0,-1):
+####    print(" "*(5-i),"*"*((2*i)-3))
+##
+##
+####for i in range(1,5,1):
+####    for j in range(1,6,1):
+####        if i==1 or i==4 or j==1 or j==5:
+####            print("*",end="")
+####        else:
+####            print(" ",end="")
+####    print()
+##
+##
+##name = "roshan is good boy123"
+##
+##print(name.upper())
+##
+##print(name.lower())
+##
+##print(name.islower())
+##
+##print(name.capitalize())
+##
+##print(name.count("a"))
+##
+##print(name.isdigit())
+##
+##print(name.isalpha())
+##
+##
+##print(name.startswith("r"))
+##
+##
+##print(name.endswith("h"))
+##
+####
+####res = name.split()
+####
+####print(res[1])
+####
+####print(len(name))
+##
+##
+##print1= name.isalnum()
+##
+##print(print1)
+##
+##
+
+
+
+
+
+
+##name = "roshan"
+##count = 0
+##for i in range(0,7,1):
+####    print(name[i])
+##    count +=1
+##
+##print(count)
+
+    
+##name = "roshan"
+##
+##for i in range(len(name)-1,-1,-1):
+##    print(name[i])
+
+
+name = "roshan"
+
+
+##print(name[-1:0])
+
+
+##res = name[5:2]
+##
+##print(res)
+
+
+
+
+
+
+
+
+####print(name[35:1:-1])
+##
+##name = "rrr"
+##
+##
+####for i in range(0,len(name),1):
+####    
+####y = name[-]:len(name)-1:-1
+####
+####if name==y:
+####    print("string is palindrome")
+##
+##
+##y = [name ,len(name),-1]
+##
+##if name == y:
+##    print("palindrome number")
+##else:
+##    print("not palindrome number")
+##
+
+
+##name = "roshan"
+##
+##
+##num = name
+##
+##c = num[5,0,-1]
+##
+##print(c)
+
+
+
+
+##name = "Roshan"
+##
+##print(name.lower())
+##print(name.upper())
+##
+
+
+
+
+
+##name = "roshan vijay  pardeshi"
+##
+##
+##a = name.split()
+##
+##print(len(a))
+##
+##
+##
+
+
+##
+##for i in range(1,3,1):
+##    print(" "*(i*i)-1,"*"*(i*i))
+
+
+
+
+##for i in range(1,5,1):
+##    print(" "*((6-i)-1),"*"*((2*i)-1))
+##
+##
+##
+##for i in range(6,1,-1):
+##    print(" "*(6-i),"*"*((2*i)-3))
+##
+
+
+
+##name = "roshan"
+##
+##count =0
+##for i in range(0,len(name),1):
+####    print(name[i])
+##    count+=1
+##
+##print(count)
+
+
+##name = "rrr"
+##
+##
+##p=name[3:0:-1]
+##
+##print(p)
+##if p==name:
+##    print(p)
+
+
+
+#### . . ###=====================================## . . ##============================## . . ##==============##..##===================
+
+
+##///////////////////////////////////////////////// ... ////////////////////////////////////////////
+
+
+##name = "roshan"
+##count = 0
+##for i in range(0,len(name),1):
+##    if name[i]=="a" or name[i]=="e" or name[i]=="i" or name[i]=="o" or name[i]=="u":
+##        count+=1
+##
+##
+##print(count)
+
+
+##name = "roshan"
+##
+##for i in range(len(name)-1,-1,-1):
+##    a = name[i]
+##    print(a,end="")
+##
+##a.split()
+##if a==name:
+##    print("palindrome")
+##else:
+##    print("not")
+
+
+import math as m
+
+
+##name = "roshan"
+
+
+##y = [name[-1]:len(name):name]
+##
+##print(y)
+
+
+
+##st = "rr"
+##
+####print(st[::-1])
+##
+##if st == st[::-1]:
+##    print("palindrome  string")
+##else:
+##    print("not a palindrome string")
+
+
+##
+##name = "roshan pardeshi"
+##
+####print(name.upper())
+####print(name.lower())
+####
+##
+##sub = "vijay"
+
+##print(name.find(sub))
+
+
+##print(name.isdigit())
+
+
+##name = "roshan pardeshi"
+##
+##
+##print(name.count("a"))
+##
+
+
+
+
+##name = "roshan vijay pardeshi"
+
+##for i in range(0,len(name),1):
+##    if name[i]==" ":
+##print(name.replace(" ","-"))
+
+##print(name)
+name = "roshan"
+
+
+print(name.upper())
+print(name.lower())
+print(name.startswith("a"))
+print(name.endswith("n"))
+print(name.capitalize())
+print(name.isdigit())
+print(name.alpha())
+
+print(name.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
