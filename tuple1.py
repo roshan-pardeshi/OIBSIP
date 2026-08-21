0288f139c811f2cae1c70d1e2087284fb6d4849b
roshan <roshanpardeshi926@gmail.com>
2026-08-21 22:47:55 +0530

    add one more

---
+++ tuple1.py
@@ -0,0 +1,217 @@
+
+
+
+##t1 = (1,2,3,4,5)
+##
+##
+##for i in t1:
+##    print(i)
+
+
+##
+##class A:
+##
+##    def __init__(self,roll_no,name):
+##        self.roll_no = roll_no
+##        self.name = name
+##
+##    def __str__(self):
+##
+##        return f"the roll_number of the a studnet:--{self.roll_no} the name of the student is a:--{self.name}"
+##
+##
+##
+##s1 = A(1,"roshan")
+##
+##s2 = A(2,"tushar")
+##
+##s3 = A(3,"manish")
+##
+##
+##li = [s1,s2,s3]
+##
+##for i in li:
+##    print(i)
+
+
+
+##
+##class A:
+##
+##
+##    def __init__(self,roll_no,name):
+##        self.roll_no = roll_no
+##        self.name = name
+##
+##
+##    def __str__(s):
+##
+##        return f"the roll no :-{s.roll_no} the name {s.name}"
+##
+##
+##s1 = A(1,"roshan")
+##
+##s2 = A(2,"sonu")
+##
+##print(s1)
+##
+##print(s2)
+
+
+
+##
+##
+##class A:
+##
+##    name = "roshan"
+##
+##    def static():
+##        print(A.name)
+##
+##    def no_st(self):
+##        print(self.name)
+##        print(self.roll_no)
+##        print(self.address)
+##
+##s1 = A()
+##s1.roll_no=43
+##s1.address="kasba peth pune"
+##s1.no_st()
+##A.static()
+##
+
+
+##
+##def main():
+##
+##    def inner():
+##        print("hello")
+##
+##    return inner
+##
+##
+##s = main()
+##
+##s()
+
+
+##
+##for i in range(97,123,1):
+##    print(chr(i))
+
+##
+##st = "roshana"
+##
+####for i in range(0,len(st),1):
+####    print(st[i])
+##
+####for i in st:
+####    print(i)
+##
+##new = ""
+##
+##
+##for i in range(0,len(st),1):
+##    if st[i] in new:
+##        pass
+##    else:
+##        new += st[i]
+##
+##print(new)
+
+
+##li = [1,2,2,3,4,5,3,2]
+##
+##
+##li1 = []
+##
+##count = 0
+##for i in range(0,len(li),1):
+##    if li[i] not in li1:
+##        li1.append(li[i])
+##        count+=1
+##    else:
+##        print(li[i])
+##
+##
+##print(li1)
+##print(count)
+
+
+##*****
+## *****
+##  *****
+##   *****
+##    *****
+##
+##for i in range(5,1,-1):
+##    print(" "*(5-i),"*"*5)
+##
+##
+##
+##
+##for i in range(0,5,1):
+##    print(" "*(5-i),"*"*5)
+##
+
+
+
+
+for i in range(3,4,1):
+    print("\n")
+    print(" "*(7-i),"*"*(2*i))
+
+
+for i in range(4,0,-1):
+    print(" "*(8-i),"*"*((2*i)-3))
+
+##
+##for i in range(4,5,1):
+##    print("*"*i,end="")
+##
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
