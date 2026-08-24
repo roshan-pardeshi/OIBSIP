14d37792c7f61b9847f0712c16a6032c2f9194d1
roshan <roshanpardeshi926@gmail.com>
2026-08-24 22:34:18 +0530

    add one more

--- A1.py
+++ A1.py
@@ -299,12 +299,14 @@
 
 
 
-num = "1023"
+##num = "1023"
+##
+##for i in num:
+##   if num[0]!="0":
+##       if int(i)==0:
+##           print("duck number")
+##
 
-for i in num:
-   if num[0]!="0":
-       if int(i)==0:
-           print("duck number")
 
 
 
@@ -344,27 +346,183 @@ for i in num:
 
 
 
+##li = [1,2,3,4,5,6]
+##
+##
+##li.append("roshan")
+##
+##print(li)
+##
+##
+##li.remove(4)
+##
+##print(li)
+##
+##li.pop()
+##
+##print(li)
+##
+##
+##li.insert(1,3)
+##
+##print(li)
+##
+##
+##print(li.index(3,0,3))
+##
+##
+##print(sorted(li))
+##
 
+##print(min(li))
+##
+##print(max(li))
+##
+##
+##st = "roshan"
+##
+##print(st.upper())
+##
+##print(st.capitalize())
+##
+##print(st.lower())
+##
+##print(st.count("a"))
+##
+##print(st.startswith("a"))
+##
+##print(st.endswith("n"))
+##
+##print(st)
 
+##
+##
+##class A:
+##
+##   def __init__(self,name,roll_no):
+##      self.name = name
+##      self.roll_no = roll_no
+##
+##
+##   def __str__(self):
+##      return f"the name is a {self.name} and roll_number is a {self.roll_no}"
+##
+##
+##a = A("roshan",1)
+##
+##print(a)
+      
+
+
+
+
+##class A:
+##
+##   def sonu(self):
+##      print("hii")
+##
+##
+##   def roshan():
+##      print("the a static are run")
+##
+##
+##
+##a = A()
+##a.sonu()
+##
+##A.roshan()
+
+##
+##class A:
+##
+##
+##   def __init__(self,name,age):
+##      self.name = name
+##      self.age = age
+##
+##   def __str__(self):
+##      return f"the name {self.name} age {self.age}"
 
+##a= A("roshan",20)
+##
+##b = A("sonu",23)
+##
+##c = A("manish",22)
+##
+##li = [a,b,c]
+##
+##for i in li:
+##   print(i)
 
 
 
 
 
 
+##st = "roshan"
+##
+##
+##for i in range(len(st)-1,-1,-1):
+##   if st[i]=="n":
+##      print(st[i])
 
 
+##st = 12
+##
+##
+##st = str(st)
+##
+##
+##if st == st[::-1]:
+##   print("palindrome")
+##else:
+##   print("not palinfrome")
 
+##st = "123"
+##
+##sum=0
+##for i in st:
+##   sum+=int(i)
+##
+##print(sum)
 
 
 
+##st = "roshan pardeshi"
+##
+##
+##st =st.split()
+##
+##
+##print(st[::-1])
 
+##for i in range(len(st)-1,-1,-1):
+##   print(st[i])
+##
 
 
+##li = [[1,2,3],
+##      [4,5,6],
+##      [7,8,9]
+##      ]
+##
+##sum=0
+##for i in range(0,len(li),1):
+##   for j in range(0,len(li[i]),1):
+##      if i+j==2:
+##         sum+=li[i][j]
+##         print(li[i][j])
+##
+##print(sum)
+##
 
 
+tu = ((1,2,3),(4,5,6),(7,8,9))
 
+for i in range(0,len(tu),1):
+   for j in range(0,len(tu[i]),1):
+      if i==j:
+         print(tu[i][j])
 
 
 
@@ -395,40 +553,47 @@ for i in num:
 
 
 
+##a = 0
+##b = 1
+##
+##
+##for i in range(51,101,1):
+##   if a>50 and a<=100:
+##      print(a)
+##   c=a+b
+##   a=b
+##   b=c
 
 
 
 
+##li = [0,1,0,2,3,4,1]
+##li1=[]
+####print(len(li))
+##for i in range(0,len(li),1):
+##   if li[i]!=0:
+##      li1.append(li[i])
+##
+####print(li1.append(0))
+##      
+##if len(li)!=len(li1):
+##   for i in range(0,100):
+##      if len(li)==len(li1):
+##         break
+##      else:
+##         li1.append(0)
+##
+##
+##print(li1)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+##num= 151
+##
+##b = str(num)
+##if b==b[::-1]:
+##   print("palindrome")
+##else:
+##   print("not")
+##
 
 
 
