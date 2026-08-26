132908cbc3c6a11f7895136308f843691ca8c8f2
roshan <roshanpardeshi926@gmail.com>
2026-08-26 21:52:51 +0530

    add one more

---
+++ set.py
@@ -0,0 +1,603 @@
+
+
+##s1 = {1,2,3,4}
+
+##print(s1)
+##
+##
+##print(type(s1))
+##
+##s1.add(20)
+##
+##print(s1)
+##
+##s1.update({20,2,3,4})
+##print(s1)
+##
+##
+##s1.remove(3)
+##
+##print(s1)
+##
+##
+##s1.pop()
+##print(s1)
+
+
+##s1.clear()
+##
+##print(s1)
+
+
+##s1 = {1,2,3,4,5,6,"roshan"}
+##s2 = {5,6,7,8,9,10}
+##
+####print(s1.union(s2))
+####print(s1.intersection(s2))
+####print(s2.difference(s1))
+##
+##
+####print(s1)
+##
+##
+####for i in s1:
+####    print(i)
+##
+##print(s1.symmetric_difference(s2))
+
+
+##s1.intersection_update(s2)
+##print(s1)
+##
+##
+##s1.difference_update(s2)
+##print(s1)
+##
+
+##s1.symmetric_difference_update(s2)
+##
+##print(s1)
+##
+
+
+##s1 = {1,2,3,4,5,6,7,8,9,10}
+
+##s1.add(10)
+##print(s1)
+##
+##s1.discard(5)
+##print(s1)
+
+##number = [1,2,3,4,2,3,1]
+##
+##
+##a = set(number)
+##
+##print(a)
+
+
+
+##s1 = "datascience"
+##
+##a = set(s1)
+##
+##for i in a:
+##    if i=="a" or i=="i" or i=="u" or i=="o" or i=="e":
+##        print(i)
+
+##s1 = {1,2,3,4,5}
+##s2 = {4,5,6,7,8}
+##
+##
+##print(s1.union(s2))
+##
+##print(s1.intersection(s2))
+##
+##print(s1.difference(s2))
+##
+##print(s1.symmetric_difference(s2))
+
+
+##s1 = {1,2}
+##s2={1,2,3,4}
+##
+##print(s1.issubset(s2))
+
+
+##c = {"roshan","tushar","neha","sita"}
+##
+##f = {"ravi sir","sita","rahul","ankit"}
+
+
+##print(c.intersection(f))
+
+
+##print(c.symmetric_difference(f))
+
+##print(c.difference(f))
+
+
+##print(f.difference(c))
+
+##print(c.symmetric_difference(f))
+
+##s1 = "hello"
+##s2 = "world"
+##
+##
+##s = set(s1)
+##s3 = set(s2)
+##
+##print(s.intersection(s3))
+
+##
+##st = "datascience"
+##
+##
+##s = set(st)
+##
+##for i in s:
+##    if i!="a" or i!="o" or i!="i" or i!="u" or i!="e":
+##        print(i)
+##
+
+
+##sq = {1,2,3,4,5,6,7,8,9,10}
+##
+##for i in sq:
+##    print(f"square--{i}:--",i**2)
+
+##st = {1,2,3,4,5,6,7}
+
+##print(min(st))
+##
+##print(max(st))
+
+
+##st = "programmming"
+##
+##
+##s = set(st)
+##
+##for i in s:
+##    print(s.union(i))
+
+
+##st = "programming"
+##
+##
+##st = set(st)
+##
+##
+##
+##for i in st:
+##    print(st.symmetric_difference(i))
+
+
+##st = "python is easy and python is powerful"
+##
+##s = set(st)
+##
+##
+##for i in s:
+##    print(list(i))
+
+
+
+##s1 = {1,2,3,4,5,6}
+##
+##s2 = {4,5,6,7,8,9}
+##
+##print(s1.symmetric_difference(s2))
+
+
+
+##st = "python is easy and python is powerful"
+##
+##st = st.split()
+##
+##st = set(st)
+
+
+##
+##print(st)
+##
+##count = 0 
+##for i in st:
+##    print(st)
+##    count+=1
+##
+##print(count)
+
+
+##
+##a = {1,2,3,4,5,6,7,8}
+##
+##for i in a:
+##    if i%2==0:
+##        pass
+##    else:
+##        print(i)
+
+##
+##se = {1,2,3,4,5,6}
+##
+##print(se.clear())
+##
+
+##
+##sq = {1,2,3,4,5}
+##
+##
+##
+##for i in sq:
+##    a = i**2
+##    
+##if a==36:
+##    print(a)
+##else:
+##    a=36
+##    print(a)
+##        
+    
+
+
+##st = {1,2,3,4,5,6}
+##
+##count = 0
+##
+##for i in st:
+##    count+=1
+##
+##    
+##print(count)
+
+##
+##s1 = [1,2,3,4,5]
+##s2 = [4,5,6,7,8]
+##
+##
+##s , e = set(s1),set(s2)
+##
+##print(s.intersection(e))
+
+##
+##a = {1,2,3,4,5,6,7,8,9,10}
+##
+##for i in a:
+##    if i%3==0:
+##        pass
+##    else:
+##        print(i)
+
+##
+##s = {1,2,3,4,5,6}
+##
+##a = 0
+##b = {}
+##for i in s:
+##    b = i**2
+##
+##
+##print(type(set(b)))
+##    
+##print(b)
+
+
+
+##a = {1,2,3,4,5,6,7,8,9}
+##
+##
+##a.add(11)
+##
+##print(a)
+
+
+
+##a.remove(4)
+##
+##print(a)
+
+##a = [1,2,33,2,11,2,1,1]
+##
+##
+##b = set(a)
+##
+##print(b)
+
+
+##
+##a = "datascience"
+##
+##
+##a = set(a)
+####
+####print(a)
+##count=0
+##for i in a:
+##    if i=="a" or i=="e" or i=="o" or i=="i" or i=="u":
+##        print(i)
+##        count+=1
+##
+##
+##print(count)
+
+##li = [1,2,1,2,3,1]
+##
+##print(li.count(2))
+
+
+
+##
+##a = {1,2,3,4,5}
+##
+##b = {4,5,6,7,8}
+##
+##
+##print(a.union(b))
+##
+##print(a.intersection(b))
+##
+##print(a.difference(b))
+##
+##
+##print(a.symmetric_difference(b))
+
+##a = {1,2,3,4}
+##
+##b = {1,2}
+##
+##
+##print(a.issubset(b))
+
+c = {"amit","ravi","neha","sita"}
+f = {"ravi","sita","rahul","ankit"}
+
+
+##print(c.union(f))
+
+##print(c.intersection(f))
+
+##print(c.difference(f))
+
+##print(f.difference(c))
+
+##st = "hello"
+##
+##st1 = "world"
+##
+##
+##st = set(st)
+##
+##st1= set(st1)
+##
+##print(st.intersection(st1))
+##
+##st = "datascience"
+##
+##
+##st = set(st)
+##
+##for i in st:
+##    if i=="a" or i=="i" or i=="e" or i=="u" or i=="o":
+##        pass
+##    else:
+##        print(i)
+
+##st = {1,2,3,4,5,6,7,8,9,10}
+##
+##for i in st:
+##    print(i,"square is a:---",i**2)
+
+
+##st = {1,2,3,4,5,6,7,8}
+##
+##
+##print(min(st))
+##print(max(st))
+
+
+
+##st = "programming"
+
+##st = set(st)
+
+##for i in st:
+##    print(st.union(i))
+
+
+##st = "roshan"
+##
+##s1 = "sonu"
+##
+##st = set(st)
+##
+##s1 = set(s1)
+##
+##
+##print(st.intersection(s1))
+
+##
+##st = "python is easy and python is powerful"
+##
+##st = st.split()
+##
+##st = set(st)
+##
+##count=0
+##
+##for i in st:
+##    print(i)
+##    count+=1
+##
+##print(count)
+
+
+##st = {1,2,3,4,5}
+##
+##
+##for i in st:
+##    if i%2==0:
+##        pass
+##    else:
+##        print(i)
+
+##st = {1,2,3,4,5,6,7}
+##
+##
+##for i in st:
+##    st.discard(i)
+
+
+##st = {1,2,3,4,5,6}
+##
+##count = 0
+##for i in st:
+##    a = i**2
+##    print(a)
+##    count+=1
+##
+##
+##print(count)
+
+##
+##
+##li = [1,2,3,4,5,6]
+##
+##l2 = [5,6,3,7,8,9]
+##
+##li = set(li)
+##
+##l2 = set(li)
+##
+##
+##print(li.intersection(l2))
+
+##st = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
+##
+##for i in st:
+##    if i%3==0:
+##        pass
+##
+##    else:
+##        print(i)
+
+##
+##class A:
+##
+##    def __init__(self,rollno,age,name):
+##        self.rollno = rollno
+##        self.age = age
+##        self.name = name
+##
+##
+##
+##    def __str__(self):
+##
+##        return f"the roll number {self.rollno} the age {self.age} the  name {self.name}"
+##
+##
+##n = int(input("enter the number to how you add the student"))
+##li = {}
+##for i in range(n):
+##    a = A(int(input("enter the rollno")),int(input("enter the age")),input("enter the a name"))
+##    
+##    li.add(a)
+##
+##
+##for i in li:
+##    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ try.py
@@ -0,0 +1,103 @@
+
+
+
+
+##print("hello")
+##
+##def A():
+##
+##    def B():
+##        print("hello")
+##
+##    return B
+##
+##
+##a = A()
+
+##class A:
+##
+##    def a(self):
+##        print("hello")
+##
+##    def c():
+##        print("nahi")
+##
+##
+##    def __init__(self,name):
+##        self.name = name
+##
+##    def d(self):
+##        return f"{self.name}"
+##
+##r = A("roshan")
+##r.d()
+##r.a()
+##A.c()
+
+
+
+
+class A:
+
+
+    def __init__(self,id,name,age):
+        self.id = id
+        self.name = name
+        self.age = age
+
+
+    def __str__(self):
+
+        return f"{self.id} {self.name} {self,age}"
+
+
+
+##a = A(1,"roshan",22)
+##
+##
+##print(a.id)
+##
+##print(a.name)
+##
+##print(a.age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
