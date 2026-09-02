ad61ea44bce7bd8011d45cc45858175d8eb43827
roshan <roshanpardeshi926@gmail.com>
2026-09-02 22:54:43 +0530

    file handling file added

--- lamdba_map_filter.py
+++ lamdba_map_filter.py
@@ -85,13 +85,160 @@
 ##
 ##print(res)
 ##
+##
+##
+##t1 = (23,33,1,2,4,5,6)
+##
+##t1 = list(t1)
+##
+##res = list(filter(lambda x:x%3==1,t1))
+##
+##print(res)
+
+##
+##
+##st =  "datascience"
+##
+##
+##d1 = {}
+##
+##for i in range(len(st)):
+##    val = st.count(st[i])
+##    d1[st[i]]=val
+##
+##print(d1.keys())
+##
+##print(d1.values())
+##
+##print(d1.items())
+##
+##d1.update({33:"ct"})
+##
+##print(d1.pop("a"))
+##
+##print(d1.popitem())
 
 
-t1 = (23,33,1,2,4,5,6)
 
-res = tuple(filter(lambda x:x,t1))
 
-print(res)
+
+##li = {1,2,3,4,5,6,7,8,9,10}
+##
+##
+##res = set(map(lambda x:x*2,li))
+##
+##print(res)
+
+
+##li = [1,4,9,16,25]
+##
+##res = set(map(lambda x:x**0.5,li))
+##
+##print(res)
+
+
+##li = {100,200,300,400,500,600}
+##
+##res = set(filter(lambda x:x>300,li))
+##
+##print(res)
+
+##li = {"a":2,"b":3,"c":12,"v":90,"t":34}
+##
+##res = dict(filter(lambda x:x[1]>10,li.items()))
+##
+##print(res)
+
+
+##li = {"x":1,"y":2,"z":3}
+##
+##res = dict(map(lambda x:(x[0],x[1]**3),li.items()))
+##
+##
+##
+##print(res)
+
+##li = {"apple":1,"banana":2,"carry":3,"kiwi":4}
+##
+##res = dict(map(lambda x:(x[0].upper(),x[1]),li.items()))
+##
+##
+##
+##print(res)
+
+##li = {"ravi":99.9,"roshan":55,"swarup":95,"manish":85}
+##
+##res = dict(filter(lambda x:x[1]>90,li.items()))
+##
+##print(res)
+
+
+##li = {"pen":100,"book":25,"pencil":5}
+##
+##res = dict(map(lambda x:(x[0],x[1]-(x[1]*10)/100),li.items()))
+##
+##print(res)
+##
+
+##li = (1,2,33,34,32,93,54,343)
+##
+##
+##res = tuple(filter(lambda x:x%10==3,li))
+##
+##print(res)
+
+
+
+##
+##st = "abcdessrfdfs"
+##
+##count = 0
+##
+##for i in range(len(st)):
+##    count+=1
+##
+##print(count)
+
+
+##a = 0
+##
+##b = 1
+##
+##num = 100
+##
+##
+##for i in range(1,num+1,1):
+##    if a>50 and a<100:
+##        print(a)
+##    c = a+b
+##    a = b
+##    b = c
+
+
+def factorial(n):
+    fact = 1
+    num=n
+    for i in  range(1,num+1,1):
+        fact=fact*i
+
+    return fact
+
+
+sum1= 0
+
+num = 145
+
+for i in str(num):
+    sum1+=factorial(int(i))
+
+
+if sum1==num:
+    print("strong number")
+
+else:
+    print("not a strong number")
+    
+
 
 
 
